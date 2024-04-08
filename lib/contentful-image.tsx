cdd4e834013e22026bff28977756c1196514b5bc
dmerkin1 <dmerkin@xtivia.com>
2024-04-08 13:02:00 -0400

    Created seperate component for featured image

--- lib/contentful-image.tsx
+++ lib/contentful-image.tsx
@@ -2,14 +2,14 @@
 
 import Image from "next/image";
 
-interface ContentfulImageProps {
+export interface ContentfulImageProps {
   src: string;
   width?: number;
   quality?: number;
   [key: string]: any; // For other props that might be passed
 }
 
-const contentfulLoader = ({ src, width, quality }: ContentfulImageProps) => {
+export const contentfulLoader = ({ src, width, quality }: ContentfulImageProps) => {
   return `${src}?w=${width}&q=${quality || 75}`;
 };
 
