baf6795d7bc71e3d72d0e60d28eb9874142461aa
dmerkin1 <dmerkin@xtivia.com>
2024-04-08 13:06:38 -0400

    Passing alt attribute to coverimage component

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -43,7 +43,7 @@ export default async function PostPage({
           )}
         </div>
         <div className="mb-8 sm:mx-0 md:mb-16">
-          <CoverImage title={post.title} url={post.coverImage.url} />
+          <CoverImage title={post.title} alt={post.title} url={post.coverImage.url} />
         </div>
         <div className="mx-auto max-w-2xl">
           <div className="mb-6 block md:hidden">
