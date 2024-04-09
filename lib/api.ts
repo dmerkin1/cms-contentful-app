7ed190c30f6f02817fdecc1c8eaa718025ce490a
dmerkin1 <dmerkin@xtivia.com>
2024-04-09 13:42:00 -0400

    Update API functions to include pagination and testimonials

--- lib/api.ts
+++ lib/api.ts
@@ -68,7 +68,7 @@ function extractPostEntries(fetchResponse: any): any[] {
 export async function getPreviewPostBySlug(slug: string | null): Promise<any> {
   const entry = await fetchGraphQL(
     `query {
-      postCollection(where: { slug: "${slug}" }, preview: true, limit: 5) {
+      postCollection(where: { slug: "${slug}" }, preview: true, limit: 10) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
@@ -79,12 +79,18 @@ export async function getPreviewPostBySlug(slug: string | null): Promise<any> {
   return extractPost(entry);
 }
 
-export async function getAllPosts(isDraftMode: boolean): Promise<any[]> {
+export async function getAllPosts(
+  isDraftMode: boolean,
+  page = 1,
+  limit = 10
+): Promise<any[]> {
+  const skip = (page - 1) * limit;
+
   const entries = await fetchGraphQL(
     `query {
       postCollection(where: { slug_exists: true }, order: date_DESC, preview: ${
         isDraftMode ? "true" : "false"
-      }) {
+      }, skip: ${skip}, limit: ${limit}) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
@@ -103,7 +109,7 @@ export async function getPostAndMorePosts(
     `query {
       postCollection(where: { slug: "${slug}" }, preview: ${
       preview ? "true" : "false"
-    }, limit: 5) {
+    }, limit: 3) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
@@ -115,7 +121,7 @@ export async function getPostAndMorePosts(
     `query {
       postCollection(where: { slug_not_in: "${slug}" }, order: date_DESC, preview: ${
       preview ? "true" : "false"
-    }, limit: 2) {
+    }, limit: 10) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
@@ -127,4 +133,21 @@ export async function getPostAndMorePosts(
     post: extractPost(entry),
     morePosts: extractPostEntries(entries),
   };
+}
+
+export async function getAllTestimonials(): Promise<any[]> {
+  const entries = await fetchGraphQL(
+    `query {
+      testimonialsCollection {
+        items {
+          name
+          content
+          image {
+            url
+          }
+        }
+      }
+    }`
+  );
+  return entries?.data?.testimonialsCollection?.items;
 }
