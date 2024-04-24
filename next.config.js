1d403d5bb64f78be152ea63d5e3cfb738b095c21
dmerkin1 <dmerkin@xtivia.com>
2024-04-24 11:52:58 -0400

    Adding dynamic routing and filtering by categories. Built UI for blogs and blog posts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
